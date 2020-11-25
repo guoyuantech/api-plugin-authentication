fbdfd855cc41d825b52782d8c1059675b378dba8
Guo Yuan <dev@gyqinzi.com>
2020-11-25 02:32:56 +0000

    Support local token

--- src/config.js
+++ src/config.js
@@ -4,7 +4,9 @@ const { str } = envalid;
 
 export default envalid.cleanEnv(process.env, {
   HYDRA_OAUTH2_INTROSPECT_URL: str({ devDefault: "http://hydra:4445/oauth2/introspect" }),
-  MOCK_TLS_TERMINATION: envalid.bool({ default: false })
+  MOCK_TLS_TERMINATION: envalid.bool({ default: false }),
+  USE_JWT_LOCAL_TOKEN: envalid.bool({ default: true }),
+  JWT_SECRET: str({ devDefault: "wrong secret" })
 }, {
   dotEnvPath: null
 });
--- src/util/expandAuthToken.js
+++ src/util/expandAuthToken.js
@@ -1,13 +1,31 @@
 import fetch from "node-fetch";
+import jwt from "jsonwebtoken";
+import Logger from "@reactioncommerce/logger";
 import config from "../config.js";
 
-const { HYDRA_OAUTH2_INTROSPECT_URL, MOCK_TLS_TERMINATION } = config;
+const { HYDRA_OAUTH2_INTROSPECT_URL, MOCK_TLS_TERMINATION, JWT_SECRET, USE_JWT_LOCAL_TOKEN } = config;
 
 let mockTlsTermination = {};
 if (MOCK_TLS_TERMINATION) {
   mockTlsTermination = {
     "X-Forwarded-Proto": "https"
   };
+}
+
+/**
+ * @param {String} token  Auth token
+ * @returns {Object} JSON object
+ */
+export function expandLocalAuthToken(token) {
+  try {
+    var tokenObj = jwt.verify(token, JWT_SECRET);
+    tokenObj.active = true;
+    tokenObj.token_type - 'access_token';
+    return tokenObj;
+  } catch (err) {
+    Logger.Error(err);
+    throw err;
+  }
 }
 
 /**
@@ -21,6 +39,10 @@ if (MOCK_TLS_TERMINATION) {
  * @returns {Object} JSON object
  */
 export default async function expandAuthToken(token) {
+  if (USE_JWT_LOCAL_TOKEN) {
+    return expandLocalAuthToken(token);
+  }
+
   const response = await fetch(HYDRA_OAUTH2_INTROSPECT_URL, {
     headers: { "Content-Type": "application/x-www-form-urlencoded", ...mockTlsTermination },
     method: "POST",
